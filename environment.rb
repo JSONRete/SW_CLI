2c8fa2619e68335a771771dc50ef6d538d9a0226
JSONRete <jsonrete@gmail.com>
2022-04-13 23:56:55 -0400

    coded my environment.rb file

---
+++ environment.rb
@@ -0,0 +1,11 @@
+
+require_relative "lib/version"
+
+
+
+require 'bundler'
+Bundler.require
+
+# require_relative "./lib/api.rb"
+# require_relative "./lib/cli.rb"
+# require_relative "./lib/planet.rb"
