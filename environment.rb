828abd650b97d1a2b477882e8829ac098b6db155
JSONRete <jsonrete@gmail.com>
2022-04-14 23:48:31 -0400

    uncommented 'require_relative's

--- environment.rb
+++ environment.rb
@@ -6,6 +6,6 @@ require_relative "lib/version"
 require 'bundler'
 Bundler.require
 
-# require_relative "./lib/api.rb"
-# require_relative "./lib/cli.rb"
-# require_relative "./lib/planet.rb"
+require_relative "./lib/api.rb"
+require_relative "./lib/cli.rb"
+require_relative "./lib/planet.rb"
