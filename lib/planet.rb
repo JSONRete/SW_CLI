03cfa512ef007f62dee0fc98f7f0433ec582aae1
JSONRete <jsonrete@gmail.com>
2022-04-16 23:23:54 -0400

    had wrong code in planet.rb file

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,10 +1,22 @@
-class API
-    
-    def self.pulled_data
-        response = RestClient.get("https://swapi.dev/api/planets")
-        planets_pull = JSON.parse(response.body)["results"]
-        planets_pull.each do |planet|
-            Planet.new(planet)
+class Planet
+    attr_accessor :name, :diameter, :population, :climate, :terrain, :rotation_period, :orbital_period
+
+    @@all = []
+
+    def initialize(attr_hash)
+        attr_hash.each do |key, value|
+            self.send("#{key}=", value) if self.respond_to?("#{key}=")
+        end
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+def self.find_by_name(name)
+        self.all.select do |planet|
+        planet.name.downcase == name
         end
     end
 end
